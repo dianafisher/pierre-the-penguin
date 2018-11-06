709c08cd1b7aefb8f02aed8d460fa965528047df
Diana Fisher <diana.fisher@gmail.com>
2018-11-06 13:35:09 -0800

    Add a blue square and set an action sequence on it.

--- Pierre The Penguin/GameScene.swift
+++ Pierre The Penguin/GameScene.swift
@@ -9,7 +9,31 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
     override func didMove(to view: SKView) {
         
+        self.anchorPoint = .zero
+        let mySprite = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
+        
+        // Set position of sprite relative to its parent node
+        mySprite.position = CGPoint(x: 150, y: 150)
+        
+        // Add it to the node tree
+        self.addChild(mySprite)
+        
+        // Scale 4X initial scale
+        let scaleAction = SKAction.scale(to: 4, duration: 5)
+        
+        // Rotate 5 radians
+        let rotateAction = SKAction.rotate(byAngle: 5, duration: 5)
+        
+        // Group the actions
+//        let actionGroup = SKAction.group([scaleAction, rotateAction])
+        
+        // Run actions in sequence
+        let actionSequence = SKAction.sequence([scaleAction, rotateAction])
+        
+        // Tell our sprite to execute the action
+        mySprite.run(actionSequence)
     }
 }
