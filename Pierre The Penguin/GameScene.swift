f437dfe17bf3e6826bc41638e69eee76613b21ce
Diana Fisher <diana.fisher@gmail.com>
2018-11-06 13:48:58 -0800

    Add bee textures and render bee node in the scene.

--- Pierre The Penguin/GameScene.swift
+++ Pierre The Penguin/GameScene.swift
@@ -13,27 +13,14 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         self.anchorPoint = .zero
-        let mySprite = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
         
-        // Set position of sprite relative to its parent node
-        mySprite.position = CGPoint(x: 150, y: 150)
+        // Set the scene background to a nice sky blue
+        self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
         
-        // Add it to the node tree
-        self.addChild(mySprite)
-        
-        // Scale 4X initial scale
-        let scaleAction = SKAction.scale(to: 4, duration: 5)
-        
-        // Rotate 5 radians
-        let rotateAction = SKAction.rotate(byAngle: 5, duration: 5)
-        
-        // Group the actions
-//        let actionGroup = SKAction.group([scaleAction, rotateAction])
-        
-        // Run actions in sequence
-        let actionSequence = SKAction.sequence([scaleAction, rotateAction])
-        
-        // Tell our sprite to execute the action
-        mySprite.run(actionSequence)
+        let bee = SKSpriteNode(imageNamed: "bee-fly")
+        bee.size = CGSize(width: 28, height: 24)
+        bee.position = CGPoint(x: 250, y: 250)
+        self.addChild(bee)
+                
     }
 }
