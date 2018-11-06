4b660bab1bcbe4ea9230e888108f1aa361b895ae
Diana Fisher <diana.fisher@gmail.com>
2018-11-06 15:37:18 -0800

    Create the GameSprite protocol.

---
+++ Pierre The Penguin/GameSprite.swift
@@ -0,0 +1,16 @@
+//
+//  GameSprite.swift
+//  Pierre The Penguin
+//
+//  Created by Diana Fisher on 11/6/18.
+//  Copyright © 2018 Diana Fisher. All rights reserved.
+//
+
+import SpriteKit
+
+protocol GameSprite {
+    var textureAtlas: SKTextureAtlas { get set }
+    var initialSize: CGSize { get set }
+    
+    func onTap()
+}
